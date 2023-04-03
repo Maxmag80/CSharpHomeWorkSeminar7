0792fe6fda17867432c1af3c06a79e2c2263e2d1
Maxim <magmax@post.com>
2023-04-03 19:56:35 +0300

    init commit

---
+++ Task2/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задача 50. Напишите программу, которая на вход принимает значение элемента в двумерном массиве,
+//и возвращает позицию этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+Console.WriteLine("input row number: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input column number: ");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input number for search: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[row, col];
+bool count = false;
+
+FillArray(array);
+PrintArray(array);
+Check(array);
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] =  new Random().Next(0, 10);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3} \t");
+        Console.WriteLine();
+    }
+}
+void Check(int[,] array)
+{
+    for (int k = 0; k < array.GetLength(0); k++)
+        for (int l = 0; l < array.GetLength(1); l++)
+        {    
+            if(array[k, l] == n)
+                count = true;
+        }
+}   
+if(count == true)
+{
+    Console.Write($"position(s) of number {n} in this array is:  ");
+    for (int o = 0; o < array.GetLength(0); o++)
+        for (int p = 0; p < array.GetLength(1); p++)
+        {    
+            if(array[o, p] == n)
+                Console.Write($"({o},{p})   ");
+        }      
+}
+else Console.WriteLine($"there is no number {n} in this array");
---
+++ Task3/Program.cs
@@ -0,0 +1,52 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+Console.WriteLine("input row number: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input column number: ");
+int col = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[row, col];
+double[] result = new double[col];
+double avg = 0;
+
+FillArray(array);
+PrintArray(array);
+Average(array);
+Console.WriteLine();
+PrintResult(result);
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] =  new Random().Next(0, 10);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"     {array[i, j],2} \t");
+        Console.WriteLine();
+    }
+}
+void Average(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+            avg = array[j, i] + avg;
+        result[i] = Math.Round(avg / 3, 2);
+        avg = 0;
+    }
+}   
+void PrintResult(double[] result)
+{
+    for (int i = 0; i < result.Length; i++)
+        Console.Write($"avg= {result[i],3} \t");
+}
